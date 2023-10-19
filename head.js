8007024df7529a7b9d70d5ec4525ef71e75c01be
Matt Morgan <matt.james.morgan97@gmail.com>
2023-10-19 23:16:19 +0000

    deleted obsolete tests

--- head.js
+++ head.js
@@ -12,7 +12,5 @@ const head = function(arr) {
   return arr[0];
 }
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
