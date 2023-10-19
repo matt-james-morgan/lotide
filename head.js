72723b133dd68803c5e3844bc46741cba884659d
Matt Morgan <matt.james.morgan97@gmail.com>
2023-10-19 23:15:11 +0000

    added head function

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+
+};
+
+const head = function(arr) {
+  return arr[0];
+}
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
