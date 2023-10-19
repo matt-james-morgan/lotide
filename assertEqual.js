0dce4a4a4d41dad8a09422ed11527b283792fa76
Matt Morgan <matt.james.morgan97@gmail.com>
2023-10-19 23:03:42 +0000

    added template literals to output

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('\u2705 Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('\uD83D\uDED1 Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`\uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 
