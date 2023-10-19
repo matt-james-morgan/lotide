fc51c0c9b622779e086d077fe1248338702bcc60
Matt Morgan <matt.james.morgan97@gmail.com>
2023-10-19 22:41:01 +0000

    created assertequal function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('\u2705 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('\uD83D\uDED1 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
