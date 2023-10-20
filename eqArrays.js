881a00b2429490b95a898401f4c8b9b33bac54e3
Matt James Morgan <122701045+matt-james-morgan@users.noreply.github.com>
2023-10-19 22:11:06 -0400

    nothing changed

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-
-};
-
-const eqArrays = function(arr1, arr2){
+const assertArraysEqual = function(arr1, arr2){
   let valid = true;
   arr1.map((x, i) =>{
     if(x !== arr2[i]){
       valid = false;
     }
   });
-  return valid;
+  if (valid) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
+  }
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
