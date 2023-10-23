867efce6eef53cc5abb3f78826e818914e66fddb
Matt Morgan <matt.james.morgan97@gmail.com>
2023-10-23 19:44:34 +0000

    added function to compare objects with primitive types and arrays

---
+++ eqObjects.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let valid = true;
+  arr1.forEach((x, i) =>{
+    if (x !== arr2[i]) {
+      valid = false;
+    }
+  });
+  return valid;
+};
+
+const eqObjects = function(obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  } else {
+    for (const key in obj1) {
+      if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+
+        if (eqArrays(obj1[key],obj2[key]) === false || obj1[key].length !== obj2[key].length) {
+          
+          return false;
+        }
+      } else if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false);
