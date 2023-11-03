342142f70600985acf3c7fe816958bba6fc75e88
Matt James Morgan <122701045+matt-james-morgan@users.noreply.github.com>
2023-11-02 20:35:57 -0400

    added recursion

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,28 @@
-const assertArraysEqual = function(arr1, arr2){
-  let valid = true;
-  arr1.map((x, i) =>{
-    if(x !== arr2[i]){
-      valid = false;
-    }
-  });
-  if (valid) {
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
     console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`\uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
   }
-}
+};
 
+const eqArrays = function(arr1, arr2) {
+  if(arr1.length !== arr2.length){return false}
+
+  for(const index in arr1){
+    if(Array.isArray(arr1[index]) && Array.isArray(arr2[index])){
+      if(!eqArrays(arr1[index], arr2[index])) {
+        return false;
+      }
+    }else if(arr1[index] !== arr2[index]){
+      return false
+    }
+  }
+  return true;
+};
+
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]) , false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
