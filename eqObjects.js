eb5602f05a32a3ceb29152dd44ef057e050afe48
Matt James Morgan <122701045+matt-james-morgan@users.noreply.github.com>
2023-11-02 20:26:03 -0400

    used recursion to solve issue with help from AI

--- eqObjects.js
+++ eqObjects.js
@@ -17,19 +17,28 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const eqObjects = function(obj1, obj2) {
-  for(const key in obj1){
-    if(typeof obj1[key] === 'object' && typeof obj2[key] === 'object' && !Array.isArray(obj1[key])){
-      return eqObjects(obj1[key], obj2[key]);
-    }else if(obj1[key]===obj2[key]){
-      return true;  
-    }else{
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if(keys1.length !== keys2.length){return false};
+
+  for(const key of keys1){
+    if(Array.isArray(obj1[key]) && Array.isArray(obj2[key])){
+      if(!eqArrays(obj1[key],obj2[key])){
+        return false;
+      }
+    }else if(typeof obj1[key] === 'object' && typeof obj2[key] === 'object'){
+      if(!eqObjects(obj1[key], obj2[key])){
+        return false;
+      } 
+    }else if(obj1[key] !== obj2[key]){
       return false;
     }
   }
   return true;
 }
 
-console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }));
+
 assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
 
 assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
