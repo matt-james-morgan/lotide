f414d2412ac50021711348486336ea3f667a78b2
Matt Morgan <matt.james.morgan97@gmail.com>
2023-11-07 17:08:58 +0000

    changed functionality of test and function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,8 @@
+const eqArrays = require("./eqArrays");
+
+
 const assertEqualArrays = function(actual, expected) {
-  if (actual === expected) {
+  if (eqArrays(actual, expected)) {
     console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`\uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
 const assertEqualArrays = require("../assertArraysEqual");
-const eqArrays = require("../eqArrays");
 
 
-assertEqualArrays(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+assertEqualArrays([1, 2, 3], [1, 2, 3]);
