f5642aa30061c2f0fa77be0ff4c8ca30ed27f48b
Matt James Morgan <122701045+matt-james-morgan@users.noreply.github.com>
2023-11-10 15:18:29 -0500

    removes tutorial.txt and fixes without.js, updated to version 1.0.1

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,11 +1,11 @@
 const assertArraysEqual = require("../assertArraysEqual");
-const assert = require("chai");
+const {assert} = require("chai");
 
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 
 describe("#assertArraysEqual", ()=>{
-  it("should return true for [1,2,3] [1,2,3]",()=>{
-    asse
+  it("should return undefined for [1,2,3] [1,2,3]",()=>{
+    assert.strictEqual(assertArraysEqual([1,2,3],[1,2,3]),undefined);
   })
 })
